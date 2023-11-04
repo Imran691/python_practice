da8764b2f4fe39e477ed20e47a5c98e7fef6f07a
Imran691 <mimran691@gmail.com>
2023-11-04 11:19:31 +0500

    class 7,8,9 added

--- prac.py
+++ prac.py
@@ -1,20 +1,4 @@
-# first_name = "muhammad's       "
-# last_name = "imran "
+name : str = input('What is your name? \t')
 
-# full_name = f"{first_name.rstrip()} {last_name}"
-
-# message = f"Hello, \n\t{full_name.title()}\n\tHow are you? \n\tI wanted to talk "
-
-# print(message)
-
-# name: list[str] = ["700", "a", 500]
-
-# name: str = 700
-
-# print(name)
-
-# print(type(name))
-# print(id(name))
-
-card: str = "Name: Imran"
-print(card)
+print(type(name))
+print(f'Welcome dear Mr/Ms {name.title()}')
